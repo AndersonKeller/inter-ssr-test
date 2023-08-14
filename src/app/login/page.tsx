9cc0df21a58dcccf00de957d8a3c65191c39cf4e
akeller <akeller@imply.com>
2023-08-14 08:43:45 -0300

    feat: includes react-hook-form and zod, validate login form

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,12 +1,34 @@
+"use client";
 import { Button } from "@/components/button/Button";
 import { Column } from "@/components/column/Column";
 import { Headline } from "@/components/headline/Headline";
 import { Label } from "@/components/label/Label";
 import { LinkComponent } from "@/components/link/Link";
 import { ModalProblems } from "@/components/modalProblems/ModalProblems";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
 import "../globals.css";
 import "./styles.css";
+const loginSchema = z.object({
+  name: z.string(),
+  password: z.string(),
+});
+type LoginData = z.infer<typeof loginSchema>;
+
 export default function Login() {
+  const [loginData, setLoginData] = useState("");
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<LoginData>({
+    resolver: zodResolver(loginSchema),
+  });
+  function login(data: LoginData) {
+    console.log(data);
+  }
   return (
     <>
       <div className="sci-login">
@@ -14,15 +36,26 @@ export default function Login() {
           <Headline type="h3" text="ENTRAR" color="red" font="cursive" />
           Escolha uma das opções abaixo para acessar
         </Column>
-        <Column>
-          <Label label="CPF, MATRÍCULA, PASSAPORTE ou DNI" />
-          <input type="text" placeholder="Somente Números e Letras"></input>
-        </Column>
-        <Column>
-          <Label label="SENHA" />
-          <input type="password"></input>
-        </Column>
-        <Button label="ENTRAR" tipo="cta-primary" size="cta-regular" />
+        <form onSubmit={handleSubmit(login)}>
+          <Column>
+            <Label label="CPF, MATRÍCULA, PASSAPORTE ou DNI" />
+            <input
+              {...register("name")}
+              type="text"
+              placeholder="Somente Números e Letras"
+            ></input>
+          </Column>
+          <Column>
+            <Label label="SENHA" />
+            <input {...register("password")} type="password"></input>
+          </Column>
+          <Button
+            type="submit"
+            label="ENTRAR"
+            tipo="cta-primary"
+            size="cta-regular"
+          />
+        </form>
         <Column className="forgot-password">
           <LinkComponent>
             <span>REDEFINIR SENHA / PRIMEIRO ACESSO SÓCIO</span>
