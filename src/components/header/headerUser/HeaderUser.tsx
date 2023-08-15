b3d15614d019edcc6a25352685ce7aa5d205c709
Anderson Keller <andersonkellervendas@hotmail.com>
2023-08-15 10:32:14 -0300

    feat: navigation to login without user context

--- src/components/header/headerUser/HeaderUser.tsx
+++ src/components/header/headerUser/HeaderUser.tsx
@@ -1,11 +1,26 @@
 "use client";
 import { useAuth } from "@/providers/user/UserProvider";
+import { useRouter } from "next/navigation";
+import { parseCookies } from "nookies";
+import { useEffect } from "react";
 import { Avatar } from "../avatar/Avatar";
 import { MiniCart } from "../miniCart/MiniCart";
 import "./styles.css";
 export function HeaderUser() {
+  const router = useRouter();
   const { user } = useAuth();
 
+  useEffect(() => {
+    const cookies = parseCookies();
+    if (cookies["@user-session-mundoColorado"]) {
+      const userSession = JSON.parse(cookies["@user-session-mundoColorado"]);
+      if (!userSession.nome) {
+        router.push("/login");
+      }
+    } else {
+      router.push("/login");
+    }
+  }, []);
   return (
     <div className="sci-header-user">
       <MiniCart />
